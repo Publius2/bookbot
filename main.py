3aa0444e2bdac5511a711c843c7d1ebdb3481152
Publius2 <mercerj.2015@gmail.com>
2024-11-20 23:39:45 -0500

    added new function to main

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ def main():
     text = get_book_text(book_path)
     num_words = wordcount(text)
     print(f"{num_words} words in book")
+    character_count = count_characters(text)
+    for x in character_count:
+        print(f"{x} : {character_count[x]}")
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,10 +15,11 @@ def count_characters(text):
     characterdict = dict()
     for x in text:
        characterdict[x.lower()] = characterdict.get(x.lower(), 0) + 1 
+    return characterdict
 
 def wordcount(text):
     words = text.split()
     return len(words)
-
+    
 
 main()
